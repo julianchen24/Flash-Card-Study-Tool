5a48f4432fa06717f41a40839be8eb5c62fb6468
Julian Chen <julianchen606@gmail.com>
2024-11-16 20:55:04 -0500

    updated display

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useState, useEffect, useRef } from 'react';
 import FlashcardList from './FlashcardList';
-import'./app.css'
+import './app.css'
 import axios from 'axios'
 
 function App() {
-  const [flashcards, setFlashcards] = useState([])
-  const [categories, setCategories] = useState([])
+  const [flashcards, setFlashcards] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-  const categoryEl = useRef()
-  const amountEl = useRef()
+  const categoryEl = useRef();
+  const amountEl = useRef();
 
   useEffect(() => {
     axios
@@ -32,60 +32,67 @@ function App() {
   function handleSubmit(e) {
     e.preventDefault()
     axios
-    .get('https://opentdb.com/api.php', {
-      params: {
-        amount: amountEl.current.value,
-        category: categoryEl.current.value
-      }
-    })
-    .then(res =>{
-      setFlashcards(res.data.results.map((questionItem, index)=>{
-        const answer = decodeString(questionItem.correct_answer)
-        const options = [
-          ...questionItem.incorrect_answers.map(a => decodeString(a)), 
-          answer]
-        return {
-          id: `${index}-${Date.now()}`,
-          question: decodeString(questionItem.question),
-          answer: answer,
-          options: options.sort(()=> Math.random() - .5)
+      .get('https://opentdb.com/api.php', {
+        params: {
+          amount: amountEl.current.value,
+          category: categoryEl.current.value
         }
-      }))
-    })
+      })
+      .then(res => {
+        setFlashcards(res.data.results.map((questionItem, index) => {
+          const answer = decodeString(questionItem.correct_answer)
+          const options = [
+            ...questionItem.incorrect_answers.map(a => decodeString(a)),
+            answer]
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(questionItem.question),
+            answer: answer,
+            options: options.sort(() => Math.random() - .5)
+          }
+        }))
+      })
 
   }
+
+  // const handleClear = (event) => {
+  //   setFlashcards([]);
+  // }
 
 
   return (
     <>
       <form className="header" onSubmit={handleSubmit}>
-        <div className = "form-group">
-          <label htmlFor="category">Category</label>
-          <select id="category" ref={categoryEl}>
-            {categories.map(category => {
-              return <option value ={category.id} key={category.id}>{category.name}
+        <div className="header-content">
+          <div className="form-group">
+            <label htmlFor="category">Category</label>
+            <select id="category" ref={categoryEl}>
+              {categories.map(category => {
+                return <option value={category.id} key={category.id}>{category.name}
 
-              </option>
-            })}
-          </select>
+                </option>
+              })}
+            </select>
+          </div>
         </div>
-        <div className = "form-group">
+        <div className="form-group">
           <label htmlFor="amount">Number Of Questions</label>
-          <input type = "number" id="amount" min="1" step="1" defaultValue={10} ref = 
-          {amountEl} />
+          <input type="number" id="amount" min="1" step="1" defaultValue={10} ref=
+            {amountEl} />
         </div>
         <div className="form-group">
           <button className="btn">Generate</button>
         </div>
-        
+        {/* <button className="clear-btn" onClick={handleClear}>Clear</button> */}
+
 
 
       </form>
-      <div className = "container">
-        <FlashcardList flashcards = {flashcards}/>
+      <div className="container">
+        <FlashcardList flashcards={flashcards} />
       </div>
     </>
-    
+
   );
 }
 
--- src/FlashcardList.js
+++ src/FlashcardList.js
@@ -4,9 +4,9 @@ import Flashcard from './Flashcard';
 export default function FlashcardList({ flashcards }) {
   return (
     <div className="card-grid">
-        {flashcards.map(flashcard =>{
-            return <Flashcard flashcard = {flashcard} key={flashcard.id}/>
-        })}
+      {flashcards.map(flashcard =>{
+          return <Flashcard flashcard = {flashcard} key={flashcard.id}/>
+      })}
     </div>
   )
 }
